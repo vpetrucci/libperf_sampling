192a74669a43b5c90831394f4fe8fd2147c47d5c
Vinicius Petrucci <vpetrucci@gmail.com>
2021-11-13 21:28:05 +0000

    working version of syscall mmap intercept using libperf

--- compile.sh
+++ compile.sh
@@ -2,7 +2,6 @@
 
 gcc -g -I/include -o sampling_mem sampling_mem.c -lperf -lpfm
 
-gcc -g -I/include -o mmap_sys_tracing mmap_sys_tracing.c
-
+gcc -g -I/include -o mmap_sys_tracing mmap_sys_tracing2.c -lperf
 
 #gcc -g -I/include -I /home/cc/linux-5.14.16/tools/lib/perf/include/ -I /home/cc/linux-5.14.16/include/ -I /usr/src/linux-headers-5.14.0-1005-oem/arch/x86/include/generated/ -o mmap_tracing2 mmap_tracing2.c -lperf
